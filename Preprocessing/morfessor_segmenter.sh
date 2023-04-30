e7bda055ad872db9161e92433ec13d2133eda71d
constantinrebouskos <c.rebouskos@gmail.com>
2023-04-30 14:58:33 +0200

    Created notebooks containing the whole code

--- Preprocessing/morfessor_segmenter.sh
+++
@@ -1,3 +0,0 @@
-morfessor -t data/tokenized-europarl-v9.fi -s data/model_full.bin
-morfessor -l data/model.bin -T - < data/tokenized-europarl-v9.fi > data/tokenized-europarl-v9.fi.segmented
-morfessor -l data/model.bin -T - --output-newlines --output-format "{analysis}  " --output-format-separator "@@ " < data/tokenized-europarl-v9.fi > data/tokenized-europarl-v9.fi.segmented
