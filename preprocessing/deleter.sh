e05d9f7ffdcb545f3a322b691fa34673dfcf53de
constantinrebouskos <c.rebouskos@gmail.com>
2023-05-02 23:21:08 +0200

    Added a script that deletes uneccesary files from the dev and test  set

---
+++ preprocessing/deleter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the directory path is provided as an argument
+if [ -z "$1" ]; then
+  echo "Please provide a directory path"
+  exit 1
+fi
+
+# Store the directory path provided as an argument
+dir="$1"
+
+# Loop through all files in the directory
+for file in "$dir"/*; do
+  # Check if the file name contains "fien" or "enfi"
+  if [[ $file != *"fien"* && $file != *"enfi"* ]]; then
+    # Delete the file if it doesn't contain the required sequence
+    rm "$file"
+    echo "Deleted file: $file"
+  fi
+done
