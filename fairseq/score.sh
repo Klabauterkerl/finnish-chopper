54a4f72839d7882bfe6f6cabf75a33bf4e2bd88c
Constantin Rebouskos <constantin.rebouskos@campus.lmu.de>
2023-06-27 15:01:18 +0200

    Deleted unnecessary scripts

--- fairseq/score.sh
+++
@@ -1,5 +0,0 @@
-# Compute BLEU score
-# Usage: bash score.sh $1 $2
-# $1: System Output
-# $2: Reference
-fairseq-score --sys $1 --ref $2 --sacrebleu
--- preprocessing/train_truecase.sh
+++
@@ -1,7 +0,0 @@
-# Train truecaser model for Finnish and English
-
-# Train truecaser model for Finnish
-mosesdecoder/scripts/recaser/train-truecaser.perl -corpus $1/train.fi -model $1/truecase-model.fi
-
-# Train truecaser model for English
-mosesdecoder/scripts/recaser/train-truecaser.perl -corpus $1/train.en -model $1/truecase-model.en
